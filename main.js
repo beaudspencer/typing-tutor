1650473ab1dda0fc419446082ba04127831b726b
Beau Spencer <bdspencer416@gmail.com>
2018-09-04 11:38:44 -0700

    move phrase variable in to applicationState object. adjusted for loop to proper syntax with new object setup. changed names of parameters in functions and adjusted functions to fit new names and variable locations. #2

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-var phrase = 'grumpy wizards make toxic brew for the evil queen'
 var applicationState = {
+  phrase: 'grumpy wizards make toxic brew for the evil queen',
   characters: [],
   currentIndex: 0
 }
+for(var i = 0; i < applicationState.phrase.length; i++) {
+  var letterObject = {
+    char: ''
+  }
+  letterObject.char = applicationState.phrase[i]
+  applicationState.characters.push(letterObject)
+}
+console.log(applicationState.characters)
 var $phraseContainer = document.getElementsByTagName('div')[1]
 
-for(var c = 0; c < phrase.length; c++) {
-  var character = {
-    letter: ''
-  }
-  character.letter = phrase[c]
-  applicationState.characters.push(character)
-}
-
-function renderChar(currentLetter, cVal) {
+function renderChar(state, letterIndex) {
   var $currentChar = document.createElement('span')
   $currentChar.classList.add('char')
-  $currentChar.textContent = currentLetter.letter
-  if( applicationState.currentIndex === cVal )
+  $currentChar.textContent = state.characters[letterIndex].char
+  if( state.currentIndex === letterIndex )
     $currentChar.classList.add('current-char')
   return $currentChar
 }
 
-function renderPhrase(phraseArray) {
+function renderPhrase(currentState) {
   var $wholePhrase = document.createElement('span')
   $wholePhrase.classList.add('whole-string')
-  for(var c = 0; c < phraseArray.length; c++){
-    $wholePhrase.appendChild(renderChar(phraseArray[c], c))
+  for(var c = 0; c < currentState.characters.length; c++){
+    $wholePhrase.appendChild(renderChar(currentState, c))
   }
   return $wholePhrase
 }
 
-$phraseContainer.appendChild(renderPhrase(applicationState.characters))
+$phraseContainer.appendChild(renderPhrase(applicationState))
